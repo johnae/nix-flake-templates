685e68ac2440c80de29f1c785a484feff8cfad7e
John Axel Eriksson <john@insane.se>
2022-04-15 10:42:45 +0200

    rust: update flake template

--- rustapp/flake.nix
+++ rustapp/flake.nix
@@ -1,51 +1,53 @@
 {
-  description = "Some Rust application";
+  description = "Bongo bong";
 
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    fenix.url = "github:nix-community/fenix";
+    nix-misc = {
+      url = "github:johnae/nix-misc";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, fenix, nixpkgs, ... }:
-    let
-      #package = pkgs: {
-      #  pname = "my-app";
-      #  version = "v0.0.0";
-      #  src = self;
-      #  cargoSha256 = "sha256-0000000000000000000000000000000000000000";
-      #  doCheck = false;
-      #  nativeBuildInputs = [ pkgs.pkgconfig ];
-      #  buildInputs = [ ];
-      #  meta = {
-      #    license = pkgs.stdenv.lib.licenses.mit;
-      #    maintainers = [
-      #      {
-      #        email = "me@myself.org";
-      #        github = "me";
-      #        name = "Me Me Me";
-      #      }
-      #    ];
-      #  };
-      #};
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-      let
-        pkgs = forAllSystems (system: import nixpkgs {
-          localSystem = { inherit system; };
-          overlays = [ fenix.overlay ];
-        });
-        rustPlatform = forAllSystems (system: pkgs.${system}.makeRustPlatform {
-          inherit (fenix.packages.${system}.minimal) cargo rustc;
-        });
-      in
-        {
-          # overlay = final: prev: {
-          #   my-app = prev.rustPlatform.buildRustPackage (package prev);
-          # };
-          # defaultPackage = forAllSystems (system: rustPlatform.${system}.buildRustPackage (package pkgs.${system}));
-          devShell = forAllSystems (system: import ./devshell.nix { pkgs = pkgs.${system}; });
+  outputs = {
+    self,
+    nixpkgs,
+    devshell,
+    nix-misc,
+    fenix,
+    flake-utils,
+  }: let
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          devshell.overlay
+          nix-misc.overlay
+          fenix.overlay
+          (final: prev: {
+            rustWithComponents = prev.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ];
+          })
+        ];
+      };
+    forAllDefaultSystems = f:
+      flake-utils.lib.eachDefaultSystem
+      (system: f system (pkgsFor system));
+  in
+    forAllDefaultSystems (
+      system: pkgs: {
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
         };
+      }
+    );
 }
